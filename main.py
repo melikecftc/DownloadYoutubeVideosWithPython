9e1577bce851df93601ea379d932632118e01a31
Melike <76225353+melikecftc@users.noreply.github.com>
2022-12-21 09:51:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occured")
+    print("Download is completed succesfully")
+
+link = input("Enter the YouTube video URL :")
+Download(link)
